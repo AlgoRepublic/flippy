7badee59ad8003a2a831b42055e2d928c6b8be1d
uudashr <uudashr@d3fc7081-4953-0410-9da1-ffcaba9ff2d6>
2008-10-11 06:07:16 +0000

    Add room module

---
+++ branches/cairngorm_version/java/src/com/flippy/service/RoomService.java
@@ -0,0 +1,48 @@
+package com.flippy.service;
+
+import java.util.List;
+
+import org.springframework.context.ApplicationContext;
+
+import com.flippy.domain.Room;
+
+/**
+ * @author uudashr
+ *
+ */
+public class RoomService {
+    public static List<Room> createRoom(String name, int learningAge, String description) {
+        RoomDAO roomDao = getRoomDao();
+        roomDao.create(name, learningAge, description);
+        return roomDao.getRooms();
+    }
+    
+    public static List<Room> getRoom() {
+        RoomDAO roomDao = getRoomDao();
+        return roomDao.getRooms();
+    }
+    
+    private static RoomDAO getRoomDao() {
+        ApplicationContext ctx = ServiceManager.getInstance().getContext();
+        RoomDAO roomDao = (RoomDAO)ctx.getBean("RoomDAO");
+        return roomDao;
+    }
+
+    public static List<Room> delete(int id) {
+        RoomDAO roomDao = getRoomDao();
+        roomDao.delete(id);
+        return roomDao.getRooms();
+    }
+
+    public static List<Room> update(int id, String name, int learningAge,
+            String description) {
+        RoomDAO roomDao = getRoomDao();
+        roomDao.update(id, name, learningAge, description);
+        return roomDao.getRooms();
+    }
+
+    public static List<Room> getRooms(int requiredLearningAge) {
+        RoomDAO roomDao = getRoomDao();
+        return roomDao.getRooms(requiredLearningAge);
+    }
+}
